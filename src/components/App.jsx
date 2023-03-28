113c2a37d0f8e08f05f87c710e00f75efd468b4d
simonphoenix67 <simonphoenix67@gmail.com>
2023-03-28 11:20:28 +0100

    001

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
+import { render } from "@testing-library/react";
+import React, {Component} from "react";
+
+export class App extends Component {
+
+  state = {
+  contacts: [],
+  name: ''
+}
+
+  // return (
+  //   <div
+  //     style={{
+  //       height: '100vh',
+  //       display: 'flex',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       fontSize: 40,
+  //       color: '#010101'
+  //     }}
+  //   >
+  //     React homework template
+  //   </div>
+  // );
+
+  render() {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+
+  )
+ }
+
+
 };
+
