3b85d179a34739ac19a45c8160e625f5e4b05033
simonphoenix67 <simonphoenix67@gmail.com>
2023-04-04 11:57:00 +0100

    fin

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -49,6 +49,8 @@ export class ContactForm extends Component {
             name="name"
             value={name}
             onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
         </label>
@@ -59,6 +61,8 @@ export class ContactForm extends Component {
             name="number"
             value={number}
             onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
         </label>
